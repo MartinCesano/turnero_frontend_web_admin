8b64bcf62644893485129fc90b2ee6380108cab4
martingaidoo <martingaido00@gmail.com>
2024-07-24 00:28:54 -0300

    se mergio manualmente la develop a la main

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, AfterViewInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,16 +16,11 @@ export class SidebarComponent {
     this.activeLink = this.router.url; // Inicialmente establecemos el enlace activo a la ruta actual
   }
 
-
   activarSidebar() {
     console.log("Activar sidebar");
     const sidebar = document.getElementById('sidebar');
     if (sidebar) {
-      sidebar.classList.toggle('translate-x-full');
-      sidebar.classList.toggle('md:translate-x-0');
-      sidebar.classList.toggle('sm:-translate-x-80');
-      sidebar.classList.toggle('sm:translate-x-0');
-      sidebar.classList.toggle('-translate-x-80');
+      sidebar.classList.toggle('show');
     } else {
       console.error("Elemento con ID 'sidebar' no encontrado.");
     }
