a449e42a96bbf4160e49bd988413d27ffb57e504
martingaidoo <martingaido00@gmail.com>
2024-08-04 14:36:17 -0300

    se agrega la funcionalidad de cambiar el estado de una reserva reservada y terminada, entre absent y present, ademas se creo el modal correspondiente

--- src/app/core/services/reservation.service.ts
+++ src/app/core/services/reservation.service.ts
@@ -4,6 +4,7 @@ import { HttpErrorResponse } from '@angular/common/http';
 import { backendUrl } from './api-environments';
 import { ModalService } from '../../components/modals/modal.service';
 import { IReservation } from '../../interfaces/reservation.interface';
+import { IState } from '../../interfaces/state.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -41,5 +42,51 @@ export class ReservationService {
       throw new HttpErrorResponse({ error });
     }
   }
+
+  async getReservationByAppointment(idAppointment: number): Promise<IReservation> {
+    try {
+      const response = await axios.get(`${backendUrl}/reservation/byAppointment/${idAppointment}`,{
+        headers: {
+          Authorization: `Bearer ${this.getAccessToken()}`
+        }
+      });
+      return response.data;  // Devuelve solo los datos de la respuesta
+    } catch (error) {
+      this.modalService.loadingClose();
+      this.modalService.openMenssageTypes({
+        text: "Error en la obtencion de las reservas.",
+        subtitle: (error as any).response.data.message,
+        url: null,
+        type: "error"
+      })
+      throw new HttpErrorResponse({ error });
+    }
+  }
+
+  async changeState(idReservation: number, state: IState): Promise<IReservation> {
+    try {
+      const response = await axios.post(`${backendUrl}/reservation/changeState`,{
+        idReservation,
+        state
+      },{
+        headers: {
+          Authorization: `Bearer ${this.getAccessToken()}`
+        }
+      });
+      return response.data;  // Devuelve solo los datos de la respuesta
+    } catch (error) {
+      this.modalService.loadingClose();
+      this.modalService.openMenssageTypes({
+        text: "Error al cambiar el estado de la reserva.",
+        subtitle: (error as any).response.data.message,
+        url: null,
+        type: "error"
+      })
+      throw new HttpErrorResponse({ error });
+    }
+  }
+
+
+
 
 }
